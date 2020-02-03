b1f995725f9e432004516ba9235c0aaa2f4b3523
Katy Abbott <40180494+katyabbott@users.noreply.github.com>
2020-02-03 17:18:07 -0600

    Update CreateMosaicDataset.py

--- CreateMosaicDataset.py
+++ CreateMosaicDataset.py
@@ -17,11 +17,11 @@ for ras in rasters:
 	month_int = int(re.search(month_expr, ras).group(0))
 	yy = int(re.search(year_expr, ras).group(0))
 	msic = mosaic_fmt.format(yy, calendar.month_abbr[month_int])
-	arcpy.management.CreateMosaicDataset("BIBE_CORE_NightRadiance.gdb", msic, 
+	arcpy.management.CreateMosaicDataset("NightRadiance.gdb", msic, 
 		"GEOGCS['GCS_WGS_1984',DATUM['D_WGS_1984',SPHEROID['WGS_1984',6378137.0,298.257223563]],\
 		PRIMEM['Greenwich',0.0],UNIT['Degree',0.0174532925199433]]",
 		None, '', "NONE", None)
-	arcpy.management.AddRastersToMosaicDataset("BIBE_CORE_NightRadiance.gdb/" + msic, "Raster Dataset", ras, 
+	arcpy.management.AddRastersToMosaicDataset("NightRadiance.gdb/" + msic, "Raster Dataset", ras, 
 		"UPDATE_CELL_SIZES", "UPDATE_BOUNDARY", "UPDATE_OVERVIEWS", None, 
 		0, 1500, None, '', "SUBFOLDERS", "ALLOW_DUPLICATES", "BUILD_PYRAMIDS", 
 		"CALCULATE_STATISTICS", "NO_THUMBNAILS", '', "NO_FORCE_SPATIAL_REFERENCE",
